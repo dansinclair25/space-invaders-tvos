22149f321c2ec468ead4b29b4d520dabb6bdf89e
Dan Sinclair <dansinclair@me.com>
2015-12-27 22:08:29 +0000

    Add ship and invaders; Move invaders

--- SpaceInvaders/GameScene.swift
+++ SpaceInvaders/GameScene.swift
@@ -9,37 +9,202 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 65
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
+    
+    enum InvaderType {
+        case A
+        case B
+        case C
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        /* Called when a touch begins */
+    enum InvaderMovementDirection {
+        case Right
+        case Left
+        case DownThenRight
+        case DownThenLeft
+        case None
+    }
+    
+    let kInvaderSize = CGSize(width:48, height:32)
+    let kInvaderGridSpacing = CGSize(width:24, height:24)
+    let kInvaderRowCount = 5
+    let kInvaderColCount = 10
+    
+    let kInvaderName = "invader"
+    let kShipSize = CGSize(width:60, height:32)
+    let kShipName = "ship"
+    
+    let kScoreHudName = "scoreHud"
+    let kHealthHudName = "healthHud"
+    
+    var contentCreated = false
+    
+    var invaderMovementDirection: InvaderMovementDirection = .Right
+    var timeOfLastMove: CFTimeInterval = 0.0
+    let timePerMove: CFTimeInterval = 1.0
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        if (!contentCreated) {
+            createContent()
+            contentCreated = true
+        }
+    }
+    
+    func createContent() {
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
+        setupInvaders()
+        setupShip()
+        setupHud()
+        
+        backgroundColor = SKColor.blackColor()
+    }
+    
+    func makeInvaderOfType(invaderType: InvaderType) -> (SKNode) {
+        
+        // 1
+        var invaderColor: SKColor
+        
+        switch(invaderType) {
+        case .A:
+            invaderColor = SKColor.redColor()
+        case .B:
+            invaderColor = SKColor.greenColor()
+        case .C:
+            invaderColor = SKColor.blueColor()
+        }
+        
+        // 2
+        let invader = SKSpriteNode(color: invaderColor, size: kInvaderSize)
+        invader.name = kInvaderName
+        
+        return invader
+    }
+    
+    func setupInvaders() {
+        
+        let baseOrigin = CGPoint(x:size.width / 3, y:self.size.height / 2)
+        for var row = 1; row <= kInvaderRowCount; row++ {
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            var invaderType: InvaderType
+            if row % 3 == 0 {
+                invaderType = .A
+            } else if row % 3 == 1 {
+                invaderType = .B
+            } else {
+                invaderType = .C
+            }
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            let invaderPositionY = CGFloat(row) * (kInvaderSize.height * 2) + baseOrigin.y
+            var invaderPosition = CGPoint(x:baseOrigin.x, y:invaderPositionY)
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            for var col = 1; col <= kInvaderColCount; col++ {
+                
+                let invader = makeInvaderOfType(invaderType)
+                invader.position = invaderPosition
+                addChild(invader)
+                
+                invaderPosition = CGPoint(x: invaderPosition.x + kInvaderSize.width + kInvaderGridSpacing.width, y: invaderPositionY)
+            }
+        }
+    }
+    
+    func setupShip() {
+        let ship = makeShip()
+        
+        ship.position = CGPoint(x:size.width / 2.0, y:kShipSize.height / 2.0)
+        addChild(ship)
+    }
+    
+    func makeShip() -> SKNode {
+        let ship = SKSpriteNode(color: SKColor.orangeColor(), size: kShipSize)
+        ship.name = kShipName
+        return ship
+    }
+    
+    func setupHud() {
+        let scoreLabel = SKLabelNode(fontNamed: "Courier")
+        scoreLabel.name = kScoreHudName
+        scoreLabel.fontSize = 50
+        
+        scoreLabel.fontColor = SKColor.whiteColor()
+        scoreLabel.text = String(format: "Score: %04u", 0)
+        
+        print(size.height)
+        scoreLabel.position = CGPoint(x: scoreLabel.frame.width / 2 + 20, y: size.height - scoreLabel.frame.size.height - 20)
+        addChild(scoreLabel)
+        
+        let healthLabel = SKLabelNode(fontNamed: "Courier")
+        healthLabel.name = kHealthHudName
+        healthLabel.fontSize = 50
+        
+        healthLabel.fontColor = SKColor.whiteColor()
+        healthLabel.text = String(format: "Health: %.1f%%", 100.0)
+        
+        healthLabel.position = CGPoint(x: frame.size.width - healthLabel.frame.width / 2 - 20, y: size.height - healthLabel.frame.size.height - 20)
+        addChild(healthLabel)
+    }
+    
+    func moveInvadersForUpdate(currentTime: CFTimeInterval) {
+        
+        if (currentTime - timeOfLastMove < timePerMove) {
+            return
+        }
+        
+        determineInvaderMovementDirection()
+        
+        enumerateChildNodesWithName(kInvaderName) {
+            node, stop in
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            switch self.invaderMovementDirection {
+            case .Right:
+                node.position = CGPoint(x: node.position.x + 10, y: node.position.y)
+            case .Left:
+                node.position = CGPoint(x: node.position.x - 10, y: node.position.y)
+            case .DownThenLeft, .DownThenRight:
+                node.position = CGPoint(x: node.position.x, y: node.position.y - 10)
+            case .None:
+                break
+            }
             
-            self.addChild(sprite)
+            self.timeOfLastMove = currentTime
+        }
+    }
+    
+    func determineInvaderMovementDirection() {
+        
+        var proposedMovementDirection: InvaderMovementDirection = invaderMovementDirection
+        
+        enumerateChildNodesWithName(kInvaderName) { node, stop in
+            switch self.invaderMovementDirection {
+            case .Right:
+                if (CGRectGetMaxX(node.frame) >= node.scene!.size.width - 1.0) {
+                    proposedMovementDirection = .DownThenLeft
+                    stop.memory = true
+                }
+            case .Left:
+                if (CGRectGetMinX(node.frame) <= 1.0) {
+                    proposedMovementDirection = .DownThenRight
+                    
+                    stop.memory = true
+                }
+            case .DownThenLeft:
+                proposedMovementDirection = .Left
+                stop.memory = true
+            case .DownThenRight:
+                proposedMovementDirection = .Right
+                stop.memory = true
+            default:
+                break
+            }
+        }
+        
+        if (proposedMovementDirection != invaderMovementDirection) {
+            invaderMovementDirection = proposedMovementDirection
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        moveInvadersForUpdate(currentTime)
     }
 }
